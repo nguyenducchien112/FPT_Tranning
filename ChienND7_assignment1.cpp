f1950822fb267098735e473bbc2ff4608a7b646b
nguyenducchien112 <51450449+nguyenducchien112@users.noreply.github.com>
2022-04-20 03:00:18 +0700

    Add files via upload

---
+++ ChienND7_assignment1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+/*Hàm tìm số đảo ngược trong C++*/
+int reverse_num(int n) {
+    int reverse = 0;
+    while (n > 0) {
+        reverse = reverse * 10 + n % 10;
+        n /= 10;
+    }
+    return reverse;
+}
+
+/*Hàm kiểm tra số đối xứng trong C++*/
+int symmetrical_num(int n) {
+    // flag = 1 => số đối xứng
+    // flag = 0 => không phải số đối xứng
+
+    int flag = 0;
+    if (reverse_num(n) == n) flag = 1;
+    return flag;
+}
+
+int main() {
+    int n;
+
+    cout << (">> Nhap so tu nhien= ");
+    cin >> n;
+
+    int check = symmetrical_num(n);
+
+    if (check == 1) cout << n << " la so doi xung";
+    else cout << n << " khong phai la la so doi xung";
+    return 0;
+}
